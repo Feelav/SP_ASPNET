b24341b8bf79d5ebd2fa40d2e771e12d08c50a81
FeelaV <koudas1009@gmail.com>
2018-11-20 16:45:56 +0100

    Adding dynamic link styling.

---
+++ SP_ASPNET_1/SP_ASPNET_1/BusinessLogic/StaticHelpers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SP_ASPNET_1.BusinessLogic
+{
+    public static class StaticHelpers
+    {
+        /// <summary>
+        /// Inspired by https://stackoverflow.com/a/20411015/9316685
+        /// </summary>
+        ///  <param name="controllers"></param>
+        /// <param name="actions"></param>
+        /// <param name="cssClass"></param>
+        /// <returns></returns>
+        public static string IsActive(this HtmlHelper html, string controllers = "", string actions = "", string cssClass = "active")
+        {
+            ViewContext viewContext = html.ViewContext;
+            bool isChildAction = viewContext.Controller.ControllerContext.IsChildAction;
+
+            if (isChildAction)
+            {
+                viewContext = html.ViewContext.ParentActionViewContext;
+            }
+
+            RouteValueDictionary routeValues = viewContext.RouteData.Values;
+            string currentAction = routeValues["action"].ToString();
+            string currentController = routeValues["controller"].ToString();
+
+            if (String.IsNullOrEmpty(actions))
+            {
+                actions = currentAction;
+            }
+
+
+            if (String.IsNullOrEmpty(controllers))
+            {
+                controllers = currentController;
+            }
+                
+
+            string[] acceptedActions = actions.Trim().Split(',');
+            string[] acceptedControllers = controllers.Trim().Split(',');
+
+            return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController) ?
+                cssClass : String.Empty;
+        }
+    }
+}
