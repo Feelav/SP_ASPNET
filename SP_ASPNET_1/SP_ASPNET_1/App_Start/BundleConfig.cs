774ec15772e023d48bd8fb378f14cc26e232abae
FeelaV <koudas1009@gmail.com>
2018-11-25 21:40:19 +0100

    transfer commit

--- SP_ASPNET_1/SP_ASPNET_1/App_Start/BundleConfig.cs
+++ SP_ASPNET_1/SP_ASPNET_1/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace SP_ASPNET_1
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                      //"~/Content/bootstrap.css",
-                     "~/Content/site.min.css"));
+                     "~/Content/css/site.min.css"));
         }
     }
 }
